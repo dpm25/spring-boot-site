5d926735c9ace4406e65e381c7c2fbb53cdf2f53
dmahoney <p!ttp@nth3r>
2016-09-23 12:11:20 -0400

    creating context of my-site

--- src/main/java/com/mahoney/App.java
+++ src/main/java/com/mahoney/App.java
@@ -2,12 +2,39 @@ package com.mahoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         SpringApplication.run(App.class,   args);
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /*private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST with Swagger")
+                .description("Spring REST with Swagger")
+                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+                .contact("Dan Mahoney")
+                .license("Apache License Version 2.0")
+                .licenseUrl("")
+                .version("2.0")
+                .build();
+    }*/
 }
