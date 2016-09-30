ad8843948abcd3c60aed9275133a85cac7092ead
dmahoney <p!ttp@nth3r>
2016-09-30 08:14:52 -0400

    more resume code

--- src/main/resources/public/js/resume-controller.js
+++ src/main/resources/public/js/resume-controller.js
@@ -1,11 +1,11 @@
-angular.module('app.resumeController', [])
-    .controller('ResumeDisplayController', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+angular.module('app.resumeController', ['ngMaterial'])
+    .controller('ResumeDisplayController', ['$scope', '$http', '$state', '$mdDialog', function ($scope, $http, $state, $mdDialog) {
 
         $scope.submitInfo = function () {
             // use $.param jQuery function to serialize data from JSON
             var data = {
-                name : $scope.name,
-                email : $scope.email
+                name: $scope.name,
+                email: $scope.email
             };
 
             var config = {
@@ -17,7 +17,8 @@ angular.module('app.resumeController', [])
             $http.post('/my-site/api/v1/resume/submit', data, config)
                 .success(function (data, status, headers) {
                     $scope.PostDataResponse = data;
-                    $state.go('resume');
+                    open();
+                    $state.go('todos');
                 })
                 .error(function (data, status, header, config) {
                     $scope.ResponseDetails = "Data: " + data +
@@ -25,5 +26,21 @@ angular.module('app.resumeController', [])
                         "<hr />headers: " + header +
                         "<hr />config: " + config;
                 });
+        };
+
+        $scope.showAlert = function(ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            // Modal dialogs should fully cover application
+            // to prevent interaction outside of dialog
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#popupContainer')))
+                    .clickOutsideToClose(true)
+                    .title('This is an alert title')
+                    .textContent('You can specify some description text in here.')
+                    .ariaLabel('Alert Dialog Demo')
+                    .ok('Got it!')
+                    .targetEvent(ev)
+            );
         };
     }]);
